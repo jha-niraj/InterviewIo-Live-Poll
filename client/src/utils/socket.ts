2a06b9db677f979e714f30cff32f783de30bf33d
jha-niraj <jhaniraj45@gmail.com>
2025-12-01 23:30:52 +0530

    update: Updated the socket_url for the client side to connect to the server

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:3000';
+const SOCKET_URL = 'https://interviewio-live-poll.onrender.com/';
 
 let socket: Socket | null = null;
 
